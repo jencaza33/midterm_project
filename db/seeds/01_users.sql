96a054c905cd3e6dcf778c94780f8c775f3d78fc
Khadeeja59 <kkausar59@gmail.com>
2022-01-10 22:39:24 +0000

    Added the tables and data

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,13 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name,phone,email,password,staff)
+VALUES
+('Alice','6230092145','alice1973@gmail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.','true');
+INSERT INTO users (name,phone,email,password,staff)
+VALUES
+('Sebastian','5230092145','seb_james@gmail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+('Charles','6450092145','Charles123@gmail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+('Oliver','5463322145','oliver@gmail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+('James','5440092145','james_keys@gmail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+('Billy','6230092145','billy_j@gmail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+('Julia','6230092145','julia_k@gmail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
