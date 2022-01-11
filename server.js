e2e44014068aa40ab938d68f5b68db4bbf8d4a16
Jen Caza <jencaza@gmail.com>
2022-01-11 14:31:11 -0500

    add styling

--- server.js
+++ server.js
@@ -1,14 +1,44 @@
-const express = require("express");
-const app = express();
-const PORT = 8080;
+"use strict";
 
-app.set("view engine", "ejs");
+require('dotenv').config();
+
+const PORT        = process.env.PORT || 8080;
+const ENV         = process.env.ENV || "development";
+const express     = require("express");
+const bodyParser  = require("body-parser");
+const sass        = require("node-sass-middleware");
+const app         = express();
 
 
+const usersRoutes = require("./routes/users");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/styles", sass({
+  src: __dirname + "/styles",
+  dest: __dirname + "/public/styles",
+  debug: true,
+  outputStyle: 'expanded'
+}));
+app.use(express.static("public"));
+
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/cart", (req, res) => {
+  res.render("cart");
+});
+
+app.get("/list-orders", (req, res) => {
+  res.render("list-orders");
+});
 
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
+  console.log("Example app listening on port " + PORT);
 });
-
-module.exports = { db };
