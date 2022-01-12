29a9288c258ab546c150c46d80fae1698f101eb8
Melvin Adu <contactmelvinadu@gmail.com>
2022-01-12 22:55:14 +0000

    refactored server and routes to use one convention after branches using two different routes conventions created multiple conflicts

--- routes/menu-router.js
+++ routes/menu-router.js
@@ -3,15 +3,11 @@ const router = express.Router();
 
 const menuRouter = (db) => {
 
-  //GET /menu/
+  //GET /order_menu/
   router.get("/", (req, res) => {
 
     const userId = req.session.user_id || '';
 
-    // User is not logged in, redirect homepage
-    if (!userId) {
-      res.redirect('/');
-    }
 
     //create query to database in order to grab all the dishes on the menu
     db.query('SELECT * FROM menu_items')
@@ -20,10 +16,9 @@ const menuRouter = (db) => {
 
     // assign session object's user_id key to user variable in order to pass it to templateVars as a variable to be used on the front end along with result of database query
     const user = req.session.user_id;
-    // const templateVars = { user, allFood };
+    const templateVars = { user, allFood };
 
-    res.json({allFood});
-    // res.render("new-order", templateVars);
+    res.render("order_menu", templateVars);
     })
     .catch(err => {
     res
