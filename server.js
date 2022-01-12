5ce46b7b60fa09931c9e50739ef3dbe813e958ab
Melvin Adu <contactmelvinadu@gmail.com>
2022-01-12 15:36:27 +0000

    add static cookie funtionality via cookie-session whenever homepage is loaded

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 
+//Cookie configuration
+const cookieSession = require('cookie-session');
+app.use(cookieSession({
+  name: 'user_id',
+  keys: ['a long long hard to crack key', 'a much longer key to crack']
+}));
+
 // PG database client/connection setup
 const { Pool } = require("pg");
 const dbParams = require("./lib/db.js");
@@ -59,12 +66,22 @@ app.use("/login", loginRouter(db));
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 app.get("/", (req, res) => {
-  res.render("index");
+
+  //set cookie upon loading homepage to 1; need to attach to database later
+  req.session["user_id"] = 1;
+
+  // assogm session object's user_id key to user variable in order to pass it to templateVars as a variable to be used on the front end
+  const user = req.session.user_id;
+  const templateVars = { user };
+
+  res.render("index", templateVars);
+
 });
 
 app.post("/", (req, res) => {
   console.log("User posted to signup");
   res.status(301).redirect("/");
+
 });
 
 app.listen(PORT, () => {
